5503bc6620d6475b7cf33cd26aeed9c2882cc803
Grafiters <bayugrafit@gmail.com>
2020-07-13 23:35:29 +0700

    testing parse to html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 $(function() {
-    var x, i;
-    var availSearch = ["ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++","Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran", "Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl", "PHP", "Python", "Ruby", "Scala", "Scheme" ];
+    var x, y, z, i;
+    var availSearch = [
+        {
+            "id":"1",
+            "title":"gunung",
+            "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
+            "img":"../img/satu.jpg"
+        },
+        {
+            "id":"2",
+            "title":"gunung",
+            "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
+            "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+        },
+        {
+            "id":"3",
+            "title":"gunung",
+            "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
+            "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+        },
+        {
+            "id":"4",
+            "title":"gunung",
+            "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
+            "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+        },
+        {
+            "id":"5",
+            "title":"gunung",
+            "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
+            "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+        },
+        {
+            "id":"6",
+            "title":"gunung",
+            "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
+            "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
+        }];
 
     $("#search").autocomplete({
         source: availSearch,
@@ -17,12 +53,25 @@ $(function() {
     $("#btn-search").click(function(){
         for(i in availSearch){
 
-            x += availSearch[i] + "</br>";
+            // var data = "<div class='col-md-4'>" +
+            //                 "<img src=" + availSearch[i].img + " class='img-fluid z-depth-1' data-toggle='modal' data-target='#modal1'/>" +
+            //             "</div>"
+            
+            // $("#img").append(data);
 
+            // // x += "title : " + availSearch[i].title + "</br>" + "link : " + availSearch[i].link + "</br>" + "img" + availSearch[i].img + "</br></br>";
+            // document.getElementById("tittle").innerHTML = availSearch[i].title;
+            // document.getElementById("link").innerHTML = availSearch[i].link;
+            // document.getElementById("img").innerHTML = availSearch[i].img;
         }
 
         // document.getElementById("data").innerHTML = x;
-        $("#data").html("result : " + x);
+        // $("#data").html("result : " + x);
+        // $("#link").html("result : " + y);
+        // $("#img").html("result : " + z);
+
+        
+
     });
 });
 
@@ -34,39 +83,5 @@ $(document).ready(function(){
     });
 });
 
-// {
-//     "id":"1",
-//     "title":"gunung",
-//     "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
-//     "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
-// },
-// {
-//     "id":"2",
-//     "title":"gunung",
-//     "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
-//     "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
-// },
-// {
-//     "id":"3",
-//     "title":"gunung",
-//     "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
-//     "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
-// },
-// {
-//     "id":"4",
-//     "title":"gunung",
-//     "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
-//     "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
-// },
-// {
-//     "id":"5",
-//     "title":"gunung",
-//     "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
-//     "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
-// },
-// {
-//     "id":"6",
-//     "title":"gunung",
-//     "link":"https://www.bromotravelindo.com/5-gunung-berapi-aktif-di-indonesia/",
-//     "img":"https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg"
-// }];
+// ["ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++","Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran", "Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl", "PHP", "Python", "Ruby", "Scala", "Scheme" ];
+
